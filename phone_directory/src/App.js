5021df619c8c16a2f7ac529a7b6b65ee325fc2c8
naviroyal <naviroyal909098@gmail.com>
2019-05-30 01:05:34 +0530

    Created Skeleton of Home Page

--- phone_directory/src/App.js
+++ phone_directory/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
 class App extends Component {
   render(){
       return (
-      <div>
-        
-      </div>
+        <div>
+          {/* <div className="header">
+            Phone Directory
+          </div>
+          <button>Add</button>
+          <div>
+            <span> Name </span><br/>
+            <span> Phone </span>
+          </div> */}
+          <label htmlFor="name">Name: </label>
+          <input id="name" type="text" placeholder="Type Here" defaultValue="Navi" />
+        </div>
+      
     );
   }
 }
