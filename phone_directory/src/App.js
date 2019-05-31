42edbf80ca233771db243a852835cbfc759b290e
naviroyal <naviroyal909098@gmail.com>
2019-05-31 22:34:10 +0530

    Passed heading to header component as props

--- phone_directory/src/App.js
+++ phone_directory/src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     ];
       return (
         <div>
-            <Header/>
+            <Header heading="Phone Directory"/>
             <div className="component-body-container">
                     <button className="custom-btn add-btn">Add</button>
 
@@ -32,6 +32,8 @@ class App extends React.Component {
                         return <div key={sub.id} className="grid-container">
                         <span className="grid-item">{sub.name}</span>
                         <span className="grid-item">{sub.phone}</span>
+                         <span className="grid-item action-btn-container">
+                          <button className="custom-btn delete-btn">Delete</button></span>
                         </div>
                       })
                     }
