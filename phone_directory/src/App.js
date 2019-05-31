7a924e63587d372efbaf1b5850e180ccf85e50df
naviroyal <naviroyal909098@gmail.com>
2019-05-31 18:24:17 +0530

    Renders subscribers dynamically

--- phone_directory/src/App.js
+++ phone_directory/src/App.js
@@ -1,21 +1,42 @@
-import React,{Component} from 'react';
-
-class App extends Component {
+import React from 'react';
+import Header from './Header';
+import './App.css'
+class App extends React.Component {
   render(){
+      
+      let subscribers = [
+      {
+          id:1,
+          name:"Navdeep",
+          phone:"9876543211"
+      },
+      {
+          id:2,
+          name:"Navi",
+          phone:"2312123442"
+      }
+    ];
       return (
         <div>
-          {/* <div className="header">
-            Phone Directory
-          </div>
-          <button>Add</button>
-          <div>
-            <span> Name </span><br/>
-            <span> Phone </span>
-          </div> */}
-          <label htmlFor="name">Name: </label>
-          <input id="name" type="text" placeholder="Type Here" defaultValue="Navi" />
-        </div>
-      
+            <Header/>
+            <div className="component-body-container">
+                    <button className="custom-btn add-btn">Add</button>
+
+                    <div className="grid-container heading-container">
+                      <span className="grid-item name-heading">Name</span>
+                      <span className="grid-item phone-heading">Phone</span>
+                    </div>
+
+                    {
+                      subscribers.map(sub=>{
+                        return <div key={sub.id} className="grid-container">
+                        <span className="grid-item">{sub.name}</span>
+                        <span className="grid-item">{sub.phone}</span>
+                        </div>
+                      })
+                    }
+            </div>
+        </div>  
     );
   }
 }
